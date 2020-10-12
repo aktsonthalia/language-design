f4828fe7cf9d2b14bedb14ef7918357a9d6912c8
Ankit Sonthalia <ankit.sonthalia01@gmail.com>
2020-10-12 21:34:53 +0530

    Fixed possible data corruption issue

--- linkedlist.c
+++ linkedlist.c
@@ -23,7 +23,7 @@ struct linked_list* createEmptyList()
 void insert(struct linked_list* list, char* data)
 {
 	struct node* new_node = (struct node*)malloc(sizeof(node));
-	new_node->data = (char*)malloc(sizeof(char));
+	new_node->data = (char*)malloc((strlen(data)+1) * sizeof(char));
 	strcpy(new_node->data, data);
 
 	if(!list->head)
