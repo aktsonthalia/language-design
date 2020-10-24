2826797dfd0df318f28f5c4207898e78b91fbd5a
Ankit Sonthalia <ankit.sonthalia01@gmail.com>
2020-10-24 13:20:21 +0530

    Added driver file

--- driver.c
+++ driver.c
@@ -1,10 +1,34 @@
 #include "utils.h"
+#include "lexer.h"
+#include "stack.h"
 #include "grammar.h"
+#include "linkedlist.h"
+#include "parsetree.h"
+#include "typeinfo.h"
+
 
 int main()
 {
-	char file[] = "grammar.txt";
+
+	// linkedList* l = createEmptyList();
+	// insert(l, "token1");
+	// insert(l, "token1");
+	// insert(l, "token1");
+	// insert(l, "token1");
+	
+	// printList(l);
 	struct grammar* g;
-	readGrammar(file, &g);
+	char grammar_file[] = "grammar.txt";
+	readGrammar(grammar_file, &g);
 	printGrammar(g);
+
+	printf("=======================================================\n");
+	
+	tokenStream* s = createEmptyTokenStream();
+	char source_file[] = "source.txt";
+	tokeniseSourcecode(source_file, s);
+	// printf("main\n");
+	printTokenStream(s);
+
+	parseTree* tree = createEmptyParseTree();
 }
